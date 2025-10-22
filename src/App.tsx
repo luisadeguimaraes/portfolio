4e95a58e31b1bbb5f5919852fad539f28c1859f3
phillip franco <pfranco266@gmail.com>
2025-10-22 16:03:19 -0700

    fix: changed sizing from px to rem for contact and components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Hero from './components/sections/Hero/Hero';
 import Header from './components/Header/Header';
 import { useTheme } from './context/ThemeContext';
 import SelectedWork from './components/SelectedWork/SelectedWork';
-import Expertise from './components/sections/Expertise/Expertise';
 import MeForReal from './components/sections/MeForReal/MeForReal';
 import Thanks from './components/sections/Thanks/Thanks';
 import Footer from './components/sections/Footer/Footer';
 import Contact from './components/sections/Contact/Contact';
+import Expertise from './components/sections/Pills/Expertise';
 
 export default function App() {
   const { theme } = useTheme(); // optional if you want to show current theme
