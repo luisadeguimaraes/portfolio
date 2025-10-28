c8953d3a9452e285b93b5dfc02a501602e9aeed9
phillip franco <pfranco266@gmail.com>
2025-10-27 18:38:43 -0700

    fix: fixed deploy settings in vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Hero from './components/sections/Hero/Hero';
 import Header from './components/Header/Header';
-import { useTheme } from './context/ThemeContext';
+// import { useTheme } from './context/ThemeContext';
 import SelectedWork from './components/SelectedWork/SelectedWork';
 import MeForReal from './components/sections/MeForReal/MeForReal';
 import Thanks from './components/sections/Thanks/Thanks';
@@ -9,7 +9,7 @@ import Contact from './components/sections/Contact/Contact';
 import Expertise from './components/sections/Pills/Expertise';
 
 export default function App() {
-  const { theme } = useTheme(); // optional if you want to show current theme
+  // const { theme } = useTheme(); // optional if you want to show current theme
 
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+// vite.config.ts
 export default defineConfig({
-  plugins: [react()],
-})
+  // ... other config
+  build: {
+    outDir: 'docs', // <-- Change 'dist' to 'docs'
+  },
+});
